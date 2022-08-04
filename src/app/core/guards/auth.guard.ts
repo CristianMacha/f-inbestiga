477fbc6dfba3b5e16e85b31a8cb264fe7f1442ed
cistianvms <c71917455@gmail.com>
2022-08-04 08:49:21 -0500

    fix auth

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { catchError, map, Observable, of, tap } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree
+} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {catchError, map, Observable, of, tap} from 'rxjs';
 
-import { appState } from '../../app.reducers';
-import { uiFeature, uiFeatureIsAuthenticate, uiFeatureIsLoading } from '../../shared/ui.selectors';
-import { AuthService } from '@core/services';
+import {appState} from '../../app.reducers';
+import {uiFeature, uiFeatureIsAuthenticate, uiFeatureIsLoading} from '../../shared/ui.selectors';
+import {AuthService} from '@core/services';
 import {loadPersonRoles, login, refreshToken, setUser} from "../../shared/ui.actions";
 
 @Injectable({
@@ -17,13 +26,14 @@ export class AuthGuard implements CanActivate, CanLoad {
     private store: Store<appState>,
     private router: Router,
     private authService: AuthService,
-  ) { }
+  ) {
+  }
 
   canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     const isAuthenticated = this.authService.refreshToken()
       .pipe(
         map((resp) => {
-          console.log(resp)
+          this.router.navigateByUrl('auth/login');
           return resp
         }),
         catchError((error) => of(false))
@@ -38,7 +48,10 @@ export class AuthGuard implements CanActivate, CanLoad {
 
     const isAuthenticated = this.authService.refreshToken()
       .pipe(
-        map((resp) => resp),
+        map((resp) => {
+          this.router.navigateByUrl('auth/login');
+          return resp;
+        }),
         catchError((error) => of(false))
       )
 
