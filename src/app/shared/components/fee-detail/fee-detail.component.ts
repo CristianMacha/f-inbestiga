c2c224287973dcfb8ee40c31a702fb35b9cd35da
BRAYANCT <brayancapchataype@gmail.com>
2022-09-13 10:51:46 -0500

    pending payment

--- src/app/shared/components/fee-detail/fee-detail.component.ts
+++ src/app/shared/components/fee-detail/fee-detail.component.ts
@@ -1,71 +1,68 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Fee, PaymentModel} from "@core/models";
-import {CFeeStatus, PaymentConceptEnum} from "@core/enums";
-import {MatDialog} from "@angular/material/dialog";
-import {DialogPayFeeComponent} from "../../dialogs/dialog-pay-fee/dialog-pay-fee.component";
-import {FeeService, PaymentService} from "@core/services";
+import { Component, Input, OnInit } from '@angular/core';
+import { Fee, PaymentModel } from '@core/models';
+import { CFeeStatus, PaymentConceptEnum } from '@core/enums';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogPayFeeComponent } from '../../dialogs/dialog-pay-fee/dialog-pay-fee.component';
+import { FeeService, PaymentService } from '@core/services';
 
 @Component({
   selector: 'vs-fee-detail',
   templateUrl: './fee-detail.component.html',
-  styleUrls: ['./fee-detail.component.scss']
+  styleUrls: ['./fee-detail.component.scss'],
 })
 export class FeeDetailComponent implements OnInit {
   @Input() fee!: Fee;
-  title:string="PENDIENTE";
+  title: string = 'PENDIENTE';
   cFeeStatus = CFeeStatus;
   payments: PaymentModel[] = [];
-  paymentStatus:any;
+  paymentStatus: any;
   constructor(
     private dialog: MatDialog,
     private paymentService: PaymentService,
-    private feeService: FeeService,
-  ) {
-  }
+    private feeService: FeeService
+  ) {}
 
   ngOnInit(): void {
     this.getPayments();
-    this.getPaymentPendient();
+    this.getPaymentPending();
   }
 
   handleRegisterNewPayment(): void {
     const dialogRef = this.dialog.open(DialogPayFeeComponent, {
       width: '500px',
-      data: {fee: this.fee},
-      autoFocus: false
+      data: { fee: this.fee },
+      autoFocus: false,
     });
 
     dialogRef.afterClosed().subscribe((resp) => resp && this.getPayments());
   }
 
   getFee(): void {
-    this.feeService.getOne(this.fee.id)
-      .subscribe((resp) => this.fee = resp);
+    this.feeService.getOne(this.fee.id).subscribe((resp) => (this.fee = resp));
   }
 
   getPayments(): void {
-    this.paymentService.getByConcept(this.fee.id, PaymentConceptEnum.FEE)
-      .subscribe((resp) => this.payments = resp);
+    this.paymentService
+      .getByConcept(this.fee.id, PaymentConceptEnum.FEE)
+      .subscribe((resp) => (this.payments = resp));
   }
 
-  updatedPayment(updated: boolean): void {
+  afterUpdatePayment(updated: boolean): void {
     this.getFee();
     this.getPayments();
-    this.getPaymentPendient();
+    this.getPaymentPending();
   }
 
-  getPaymentPendient():void{
+  getPaymentPending(): void {
     let sum = 0;
-    this.paymentService.getByConcept(this.fee.id, PaymentConceptEnum.FEE)
-    .subscribe((resp) => {
-      resp.forEach((element) => {
-        if (element.status=="VERIFICADO" ) {
-          if (element.conceptId=element.conceptId) {
-            this.paymentStatus = sum+=element.amount;    
+    this.paymentService
+      .getByConcept(this.fee.id, PaymentConceptEnum.FEE)
+      .subscribe((resp) => {
+        resp.forEach((payment) => {
+          if (payment.status == 'VERIFICADO') {
+            this.paymentStatus = sum += payment.amount;
           }
-        }        
+        });
       });
-    });
-    
   }
 }
