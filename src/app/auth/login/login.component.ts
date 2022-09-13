fc7123a377b0bfc03642471e61a8ab91be9fad9b
BRAYANCT <brayancapchataype@gmail.com>
2022-09-13 16:55:37 -0500

    login validate

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Store} from '@ngrx/store';
-import {debounceTime, finalize, Observable, Subscription, takeUntil} from 'rxjs';
+import { finalize, Subscription} from 'rxjs';
 
-import {uiFeatureIsLoading} from '../../../app/shared/ui.selectors';
 import {loadRoleSelected, logout} from '../../shared/ui.actions';
 import {AppStateAuthFeature} from '../store/auth.reducer';
 import {AuthService} from "@core/services";
