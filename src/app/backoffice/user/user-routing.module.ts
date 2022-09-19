55e4063cc447af40b10227f83d91412d1228d3e9
BRAYANCT <brayancapchataype@gmail.com>
2022-09-19 08:42:37 -0500

    route user

--- src/app/backoffice/user/user-routing.module.ts
+++ src/app/backoffice/user/user-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 import { UserComponent } from './user.component';
 
@@ -11,6 +13,17 @@ const routes: Routes = [
         path: '',
         component: UserComponent,
         title: 'Usuarios'
+      },
+      {
+        path: ':id',
+        component: UserFormComponent,
+        title: 'Actualizar del Usuario'
+      }
+      ,
+      {
+        path: 'detalle/:id',
+        component: UserDetailComponent,
+        title: 'Detalles del Usuario'
       }
     ]
   }
