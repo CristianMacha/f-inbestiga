37e7162b84d56485552715560fd8f25f4bc8ce62
cristianv.ms <73314207+CristianMacha@users.noreply.github.com>
2022-12-05 10:11:18 -0500

    fix edit total invoice

--- src/app/shared/components/invoice-detail/invoice-detail.component.ts
+++ src/app/shared/components/invoice-detail/invoice-detail.component.ts
@@ -1,33 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from "@angular/router";
 
-import { Invoice } from "@core/models";
+import { Invoice, Role } from "@core/models";
 import { CInvoiceStatus } from "../../../core/enums/invoice.enum";
-import { CFeeStatus } from '@core/enums';
+import { CFeeStatus, CRole } from '@core/enums';
 import { DialogInvoiceEditComponent } from '../../dialogs/dialog-invoice-edit/dialog-invoice-edit.component';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { appState } from '../../../../app/app.reducers';
+import { uiRoleSelected } from '../../ui.selectors';
 
 @Component({
   selector: 'vs-invoice-detail',
   templateUrl: './invoice-detail.component.html',
   styleUrls: ['./invoice-detail.component.scss']
 })
-export class InvoiceDetailComponent implements OnInit {
+export class InvoiceDetailComponent implements OnInit, OnDestroy {
   @Input() showMembers: boolean = true;
   @Input() showInfoPayment: boolean = true;
   @Input() invoice: Invoice = new Invoice();
   @Input() showDetail: boolean = false;
 
+  subscription = new Subscription();
+
   cInvoiceStatus = CInvoiceStatus;
   cFeeStatus = CFeeStatus;
+  cRole = CRole;
+  roleSelected = new Role();
 
   constructor(
     private router: Router,
     private matDialog: MatDialog,
+    private store: Store<appState>,
   ) {
   }
 
   ngOnInit(): void {
+    this.getRoleSelected();
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
+  }
+
+  getRoleSelected(): void {
+    this.subscription.add(
+      this.store.select(uiRoleSelected).subscribe((role) => this.roleSelected = role)
+    )
   }
 
   handleViewDetails(): void {
