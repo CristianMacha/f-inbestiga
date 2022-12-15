c4bfce43c5158f5d44cf09fe313b980e78b14ec7
cristianv.ms <73314207+CristianMacha@users.noreply.github.com>
2022-12-15 15:45:55 -0500

    update email

--- src/app/backoffice/user/components/user-form/user-form.component.ts
+++ src/app/backoffice/user/components/user-form/user-form.component.ts
@@ -15,7 +15,7 @@ import { RoleService } from 'src/app/core/services/role.service';
 @Component({
   selector: 'vs-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.scss']
+  styleUrls: ['./user-form.component.scss'],
 })
 export class UserFormComponent implements OnInit, OnDestroy {
   separatorKeysCodes: number[] = [ENTER, COMMA];
@@ -23,7 +23,6 @@ export class UserFormComponent implements OnInit, OnDestroy {
 
   personRolesSelected: PersonRoles[] = [];
   roles: Role[] = [];
-
   personForm: FormGroup = new FormGroup({
     id: new FormControl(0, Validators.required),
     fullName: new FormControl('', Validators.required),
@@ -31,12 +30,15 @@ export class UserFormComponent implements OnInit, OnDestroy {
     phone: new FormControl('', [Validators.required, Validators.minLength(7)]),
     user: new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(7)]),
-    })
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    }),
   });
 
   requirements: User[] = [];
-  personId:number=0;
+  personId: number = 0;
   title: string = 'Nuevo usuario';
   btnActionText: string = 'Crear usuario';
 
@@ -46,21 +48,20 @@ export class UserFormComponent implements OnInit, OnDestroy {
   person: Person = new Person();
 
   @ViewChild('roleInput') roleInput!: ElementRef<HTMLInputElement>;
-  @ViewChild("chipList") chipList: any;
+  @ViewChild('chipList') chipList: any;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private  personService:PersonService,
+    private personService: PersonService,
     private router: Router,
     private dialog: MatDialog,
-    private roleServices: RoleService,
-  ) {
-  }
+    private roleServices: RoleService
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(resp=>{
-      this.personId=parseInt(resp['id']);
-      if(this.personId){
+    this.activatedRoute.params.subscribe((resp) => {
+      this.personId = parseInt(resp['id']);
+      if (this.personId) {
         this.title = 'Actualizar Usuario';
         this.btnActionText = 'Actualizar Usuario';
         this.checkFormStatus(this.personId);
@@ -75,9 +76,10 @@ export class UserFormComponent implements OnInit, OnDestroy {
 
   getRoles(): void {
     this.loading = true;
-    this.roleServices.getAll()
-      .pipe(finalize(()=> this.loading = false))
-      .subscribe((resp) => this.roles = resp);
+    this.roleServices
+      .getAll()
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe((resp) => (this.roles = resp));
   }
 
   handleBtnCancel() {
@@ -85,52 +87,84 @@ export class UserFormComponent implements OnInit, OnDestroy {
   }
 
   hanldeCreate() {
-    this.verifyPersonRoles()
-    if(this.personRolesSelected.length == 0) { return }
-    this.personForm.invalid ? this.personForm.markAllAsTouched() : this.createUser();
+    this.verifyPersonRoles();
+    if (this.personRolesSelected.length == 0) {
+      return;
+    }
+    this.personForm.invalid
+      ? this.personForm.markAllAsTouched()
+      : this.createUser();
   }
 
   createUser(): void {
-    this.personService.create({...this.personForm.value, personRoles: this.personRolesSelected}).subscribe(resp=>{
-      this.router.navigateByUrl(`backoffice/user`).then();
-    });
+    this.loading = true;
+    this.personService
+      .create({
+        ...this.personForm.value,
+        personRoles: this.personRolesSelected,
+      })
+      .subscribe((resp) => {
+        this.router.navigateByUrl(`backoffice/user`).then();
+        this.loading = false;
+      });
   }
 
   handleUpdate() {
-    this.verifyPersonRoles()
-    if(this.personRolesSelected.length == 0) { return }
-    this.personForm.invalid ? this.personForm.markAllAsTouched() : this.updateUser();
+    this.verifyPersonRoles();
+    if (this.personRolesSelected.length == 0) {
+      return;
+    }
+    this.personForm.invalid
+      ? this.personForm.markAllAsTouched()
+      : this.updateUser();
   }
 
   updateUser(): void {
-    this.personService.update({...this.personForm.value, personRoles: this.personRolesSelected}).subscribe(resp=>{
-      this.router.navigateByUrl(`backoffice/user`).then();
+    this.loading = true;
+    this.personService
+      .update({
+        ...this.personForm.value,
+        personRoles: this.personRolesSelected,
+        user: {
+          email: (this.personForm.controls['user'] as FormGroup).controls[
+            'email'
+          ].value,
+        },
+      })
+      .subscribe(() => {
+        this.router.navigateByUrl(`backoffice/user`).then();
+        this.loading = false;
+      });
+  }
+
+  checkFormStatus(personId: number) {
+    this.personService.getByUser(personId).subscribe((resp) => {
+      this.editMode = true;
+      this.personForm.patchValue(resp);
+      (this.personForm.controls['user'] as FormGroup).controls[
+        'password'
+      ].disable();
+      resp.personRoles.forEach((personRole) =>
+        this.personRolesSelected.push(personRole)
+      );
     });
   }
 
-  checkFormStatus(personId:number) {
-    this.personService.getByUser(personId)
-    .subscribe((resp)=>{
-      this.editMode = true;
-      this.personForm.patchValue(resp);
-      (this.personForm.controls['user'] as FormGroup).controls['email'].disable();
-      (this.personForm.controls['user'] as FormGroup).controls['password'].disable();
-      resp.personRoles.forEach((personRole) => this.personRolesSelected.push(personRole))
-    })
-  }
-
-  updatePassword(){
+  updatePassword() {
     this.dialog.open(DialogUserPasswordComponent, {
       width: '500px',
-      data: { personId: this.personId},
+      data: { personId: this.personId },
       disableClose: true,
     });
   }
 
   remove(personRole: PersonRoles): void {
-    const index = this.personRolesSelected.findIndex((pr) => pr.id == personRole.id);
-    if(this.editMode && personRole.id !== 0) {
-      this.personRolesSelected[index].active = !this.personRolesSelected[index].active;
+    const index = this.personRolesSelected.findIndex(
+      (pr) => pr.id == personRole.id
+    );
+    if (this.editMode && personRole.id !== 0) {
+      this.personRolesSelected[index].active =
+        !this.personRolesSelected[index].active;
     } else {
       if (index >= 0) {
         this.personRolesSelected.splice(index, 1);
@@ -146,13 +180,15 @@ export class UserFormComponent implements OnInit, OnDestroy {
     };
 
     const newPersonRole: any = {
-      id:0,
-      role:newRole,
-      active:true,
+      id: 0,
+      role: newRole,
+      active: true,
     };
 
-    const index = this.personRolesSelected.findIndex((personRole) => personRole.role.id == newRole.id);
-    if(index < 0) {
+    const index = this.personRolesSelected.findIndex(
+      (personRole) => personRole.role.id == newRole.id
+    );
+    if (index < 0) {
       this.personRolesSelected.push(newPersonRole);
       this.verifyPersonRoles();
     }
@@ -162,7 +198,7 @@ export class UserFormComponent implements OnInit, OnDestroy {
   }
 
   verifyPersonRoles(): void {
-    if(this.personRolesSelected.length == 0) {
+    if (this.personRolesSelected.length == 0) {
       this.chipList.errorState = true;
     } else {
       this.chipList.errorState = false;
